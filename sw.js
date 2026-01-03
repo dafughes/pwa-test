658e13b7cf09822b87ed45cef6f5025fce090017
Samuel Hughes <samuel.hughes@aalto.fi>
2026-01-03 16:24:38 +0200

    stuff

--- sw.js
+++ sw.js
@@ -1,51 +1,65 @@
-var GHPATH = '/pwa-test';
-var APP_PREFIX = 'gppwa_';
-var VERSION = 'version_002';
-var URLS = [    
-  `${GHPATH}/`,
-  `${GHPATH}/index.html`,
-  `${GHPATH}/js/app.js`,
-  `${GHPATH}/icon.png`
-]
+// The version of the cache.
+const VERSION = "v1";
 
-var CACHE_NAME = APP_PREFIX + VERSION
-self.addEventListener('fetch', function (e) {
-  console.log('Fetch request : ' + e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { 
-        console.log('Responding with cache : ' + e.request.url);
-        return request
-      } else {       
-        console.log('File is not cached, fetching : ' + e.request.url);
-        return fetch(e.request)
+// The name of the cache
+const CACHE_NAME = `pwa-test-${VERSION}`;
+
+// The static resources that the app needs to function.
+const APP_STATIC_RESOURCES = [
+  "/",
+  "/index.html",
+  "/js/app.js",
+  "/icon.png",
+];
+
+// On install, cache the static resources
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(APP_STATIC_RESOURCES);
+    })(),
+  );
+});
+
+// delete old caches on activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const names = await caches.keys();
+      await Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+          return undefined;
+        }),
+      );
+      await clients.claim();
+    })(),
+  );
+});
+
+// On fetch, intercept server requests
+// and respond with cached responses instead of going to network
+self.addEventListener("fetch", (event) => {
+  // As a single page app, direct app to always go to cached home page.
+  if (event.request.mode === "navigate") {
+    event.respondWith(caches.match("/"));
+    return;
+  }
+
+  // For all other requests, go to the cache first, and then the network.
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(event.request.url);
+      if (cachedResponse) {
+        // Return the cached response if it's available.
+        return cachedResponse;
       }
-    })
-  )
-})
-
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('Installing cache : ' + CACHE_NAME);
-      return cache.addAll(URLS)
-    })
-  )
-})
-
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
-      })
-      cacheWhitelist.push(CACHE_NAME);
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('Deleting cache : ' + keyList[i] );
-          return caches.delete(keyList[i])
-        }
-      }))
-    })
-  )
-})
+      // If resource isn't in the cache, return a 404.
+      return new Response(null, { status: 404 });
+    })(),
+  );
+});
